0e19d5a02411e90294d8f853d5368ba362f284ac
Trần Nguyễn Duy Thiện <94496525+LaderHasobor@users.noreply.github.com>
2024-11-04 14:38:09 +0700

    Thêm thư mục firstapp với các cấu hình và mã nguồn ban đầu

---
+++ firstapp/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      {/* Các nội dung khác của trang */}
+    </div>
+  );
+}
+
+export default App;
